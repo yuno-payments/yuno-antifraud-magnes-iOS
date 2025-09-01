7f4bced00e643c58146f8e14a79a602202b3ceb8
Andres Montoya <andres.montoya@y.uno>
2025-09-01 11:26:17 -0500

    Update/version (#2)
    * new release
    
    * new release
    
    * change version

--- Package.swift
+++ Package.swift
@@ -3,25 +3,24 @@ import PackageDescription
 
 let package = Package(
     name: "YunoAntifraudMagnes",
+    defaultLocalization: "en",
     platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "YunoAntifraudMagnes",
-            targets: ["PPRiskMagnes", "MagnesWrapper"])
+            targets: ["YunoAntifraudMagnes", "PPRiskMagnes"])
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
+            name: "YunoAntifraudMagnes",
+            url: "https://github.com/yuno-payments/yuno-antifraud-magnes-iOS/releases/download/v1.0.0/YunoAntifraudMagnes.xcframework.zip",
+            checksum: "93c470f4dc6e0e7e2df7af3438a860c924e5953e9e4be57bbe08da68b2b45836"
+        ),
+        .binaryTarget(
             name: "PPRiskMagnes",
             url: "https://github.com/yuno-payments/yuno-antifraud-magnes-iOS/releases/download/v1.0.0/PPRiskMagnes.xcframework.zip",
             checksum: "cc0d84ba2290239de0456e257d5d8ad60a9644d24eaf2fddca56761a9cd201d5"
-        ),
-        .target(
-            name: "MagnesWrapper",
-            dependencies: [
-                .target(name: "PPRiskMagnes")
-            ],
-            path: "MagnesWrapper"
         )
     ]
 )
